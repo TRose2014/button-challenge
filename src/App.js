1fde61262a9bb67becff88fa3536360936f37072
TRose2014 <shortyshort789@aol.com>
2021-10-12 19:08:41 -0700

    added bootstrap react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+// import Button from './button/button'
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="secondary" size="lg">Secondary</Button>
+
+      <Button variant="outline-primary">Primary</Button>
+
+      <Button variant="outline-danger">Danger</Button>
+
+      <Button variant="danger">Danger</Button>
     </div>
   );
 }
